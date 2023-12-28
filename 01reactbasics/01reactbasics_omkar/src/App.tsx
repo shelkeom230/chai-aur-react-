af0c68b94df532707e5140aa8e967b136b57708d
omkar <shelkeom230@gmail.com>
2023-12-28 01:30:59 -0800

    completed with tutorial 2

---
+++ 01reactbasics/01reactbasics_omkar/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+
+  return (
+    <h1>chai aur react with vite || HC and omkar shelke</h1>
+    
+  )
+}
+
+export default App
---
+++ 01reactbasics/src/App.js
@@ -0,0 +1,9 @@
+// import './App.css';
+
+function App() {
+  return (
+    <h1>Omkar Shelke || chai aur react</h1>
+  );
+}
+
+export default App;
